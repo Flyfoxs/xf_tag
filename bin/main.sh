8e4846ad7177b01f90ca05a437d004cf268b6050
lali2 <lali2@cisco.com>
2019-07-21 08:42:15 +0800

    default value for $1

--- bin/main.sh
+++ bin/main.sh
@@ -2,11 +2,15 @@
 cd "$(dirname "$0")"
 cd ..
 
-for i in {1..100};
+##for i in {1..10};
+#for i in $(seq 0 $1)
+
+#if not input $1, default value is 100
+for i in $(seq 0 ${1:-100})
 do
     for fold in {0..4};
     do
-        python -u ./core/bert.py --fold ${fold} train_base  >> batch_fold_${fold}.log 2>&1
+        python -u ./core/bert.py --fold ${fold} train_base  >> batch_fold_${fold}_"$(hostname)".log 2>&1
     done
 done
 
