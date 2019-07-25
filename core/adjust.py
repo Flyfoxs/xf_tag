78754ffd752cc0e1713d6546f75e7503c7f94fd9
lali2 <lali2@cisco.com>
2019-07-25 22:12:44 +0800

    81.42146 online, base on 81.57537

--- core/adjust.py
+++ core/adjust.py
@@ -1,7 +1,22 @@
 
 from pandas import DataFrame as DF
-from core.conf import num_classes
 
+
+from core.conf import num_classes
+from core.ensemble import *
+
+adjust_col = [
+'140901',
+'142302',
+'142701',
+'140601',
+'140206',
+'142601',
+'140210',
+'140211',
+'142105',
+'140207'
+]
 class OptimizedRounder(object):
     def __init__(self):
         self.coef_ = 0
@@ -9,34 +24,60 @@ class OptimizedRounder(object):
         self.best_score = 0
         self.initial_score = 0
         self.val_score = []
-        self.initial_coef = [1] * num_classes
+        self.initial_coef = [.88530247, 1.09971672, 0.91296856, 1.42351244, 1.30119761,
+                             1.67477818,1.13638098, 1.38549708, 1.02128626, 1.47433436]
 
-    def _kappa_loss(self, coef, X, y):
-        X_p = DF(np.copy(X))
-        for i in range(len(coef)):
-            X_p[i] *= coef[i]
+    #@timed()
+    def _kappa_loss(self, coef, X):
+        X = X.copy()
 
-        l1 = f1_score(y, np.argmax(X_p.values, axis=1), average="weighted")
-        self.coef_arr.append(coef)
-        self.best_score = max(l1, self.best_score)
-        print(list(coef.astype(np.float16)), ' Train score = ', l1.astype(np.float32), 'Best Score',
-              self.best_score)  # ,' Valid score =',l2.astype(np.float16))
-        return -l1
+        for col, adjust in zip(adjust_col, coef):
+            X.loc[:, col] = X.loc[:, col] * adjust
 
-    def fit(self, X, y):
-        self.initial_score = f1_score(y, np.argmax(X.values, axis=1), average="weighted")
+        acc1, acc2, total = accuracy(X)
+        logger.info(f'acc1:{acc1:7.6f}, acc2:{acc2:7.6f}, total:{total:7.6f} with:{coef}')
+        return -round(total,6)
 
-        loss_partial = partial(self._kappa_loss, X=X, y=y)
-        self.coef_ = sp.optimize.minimize(loss_partial, self.initial_coef, method='Powell')
+    def fit(self, X):
+        self.initial_score = accuracy(X)
+        logger.info(f'initial_score:{self.initial_score}')
+        from functools import partial
+        loss_partial = partial(self._kappa_loss, X=X)
+        import scipy
+        self.coef_ = scipy.optimize.minimize(loss_partial, self.initial_coef, method='Powell')
 
-    def predict(self, X, coef):
+    def predict(self, test, coef):
 
         logger.info(f'Predict with:{coef}')
-        #print(self.coef_)
-        X_p = DF(np.copy(X))
-        for i in range(len(coef)):
-            X_p[i] *= coef[i]
-        return X_p
+        test = test.copy()
+        for col, adjust in zip(adjust_col, coef):
+            test.loc[:, col] = test.loc[:, col] * adjust
+        return test
 
     def coefficients(self):
         return self.coef_['x']
+
+
+
+if __name__ == '__main__':
+    top=2
+    opt = OptimizedRounder()
+    oof = get_feature_oof(top)
+    train = oof.loc[oof.label != '0']
+    opt.fit(oof)
+    coef = opt.coefficients()
+    logger.info(f'Best coef:{coef}')
+
+    test = oof.loc[oof.label == '0']
+    for col, adjust in zip(adjust_col, coef):
+        test.loc[:, col] = test.loc[:, col] * adjust
+
+    gen_sub_file(test, f'mean_adjust_{top}.csv')
+
+
+
+
+
+""""
+nohup python -u ./core/adjust.py > adjust.log 2>&1 &
+"""
